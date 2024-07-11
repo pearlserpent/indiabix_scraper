3643ee6f1a1be20fa5acd389ebedd113ff4e6d87
pearlserpent <ismailmaj.23@gmail.com>
2024-07-11 12:00:40 +0000

    ADFD

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        "https://quotes.toscrape.com/tag/humor/",
+        "https://www.indiabix.com/aptitude/problems-on-trains",
     ]
 
     def parse(self, response):
